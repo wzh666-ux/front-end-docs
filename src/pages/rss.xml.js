f0caae3e15d2c7032995b90e8b56e2a10fb9e6c9
王政和 <ccoderux@163.com>
2024-03-17 13:15:01 +0800

    feat: 更新rss引用

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,12 +3,22 @@ import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
+	const postsMarkdown = await getCollection('markdown');
+	const postsVue = await getCollection('vue');
+	const postsJenkins = await getCollection('jenkins');
+	const postsDocker = await getCollection('docker');
+	const postsReact = await getCollection('react');
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
+		items: [
+			...postsMarkdown,
+			...postsVue,
+			...postsJenkins,
+			...postsDocker,
+			...postsReact
+		].map((post) => ({
 			...post.data,
 			link: `/blog/${post.slug}/`,
 		})),
