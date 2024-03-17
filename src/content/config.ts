6967b8d6f4439f7142d68fe727f9f1844393268e
王政和 <ccoderux@163.com>
2024-03-17 13:11:48 +0800

    feat: 新增两篇文档。

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,6 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
+const markdown = defineCollection({
 	type: 'content',
 	// Type-check frontmatter using a schema
 	schema: z.object({
@@ -13,6 +13,54 @@ const blog = defineCollection({
 	}),
 });
 
+const vue = defineCollection({
+	type: 'content',
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+	})
+})
+
+const jenkins = defineCollection({
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+	})
+})
+
+const react = defineCollection({
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+	})
+})
+
+const docker = defineCollection({
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+	})
+})
+
 const home = defineCollection({
 	type: "content",
 	schema: z.object({
@@ -20,13 +68,4 @@ const home = defineCollection({
 		description: z.string(),
 	})
 })
-
-const about = defineCollection({
-	type: "content",
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-	})
-})
-
-export const collections = { blog, home, about };
+export const collections = { markdown, home, vue, jenkins, react, docker };
